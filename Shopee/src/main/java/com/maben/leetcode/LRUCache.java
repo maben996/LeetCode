0333dfde5ca72bfe315f4f71d133e0b28438244b
maben10 <maben9.jd.com>
2021-03-03 01:00:38 +0800

    LRU缓存的理解：每次调用会把当前命中缓存的放在最近的位置，按照时间顺序和总缓存容量删除掉最远（早）的那个 链表的赋值：从左往右看

---
+++ Shopee/src/main/java/com/maben/leetcode/LRUCache.java
@@ -0,0 +1,93 @@
+package com.maben.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * 实现LRUCache
+ */
+public class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+        public DLinkedNode() {}
+        public DLinkedNode(int key, int value) {this.key= key; this.value = value;}
+    }
+
+    private HashMap<Integer,DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        // 使用伪头部节点和伪尾部节点
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+        // 如果key存在，将node移动到链表的头部（head之后）
+        removeToHead(node);
+        addToHead(node);
+        return node.value;
+    }
+
+    /**
+     * 存入key-value，如果key存在则更新value，并移动该node节点到链表头部，
+     * 如果key不存在，则新增一个值为value的node到链表头部，
+     * 检查链表长度是否超出capacity，超出则删除链表最后一个node
+     * @param key
+     * @param value
+     */
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if (node != null) {
+            node.value = value;
+            // 将node移动到链表的头部（head之后）
+            removeToHead(node);
+            addToHead(node);
+        } else {
+            // key 不存在，则创建node，存入hash表
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            cache.put(key, newNode);
+            // 将新node移动到链表的头部（head之后）
+            addToHead(newNode);
+            ++size;
+            // 如果超出缓存容量，则去除掉最后一个node
+            if (size > capacity) {
+                // 从hash表中删除最后一个node的key
+                cache.remove(tail.prev.key);
+                // 从链表中去除最后一个node
+                removeToHead(tail.prev);
+                --size;
+            }
+        }
+    }
+
+    /**
+     * 将node添加到链表的头部（head之后）
+     * @param node
+     */
+    private void addToHead(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    /**
+     * 将node从链表中去掉
+     */
+    private void removeToHead(DLinkedNode node) {
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+    }
+}
+
---
+++ Shopee/src/main/java/com/maben/leetcode/question160.java
@@ -0,0 +1,22 @@
+package com.maben.leetcode;
+
+
+/**
+ * 编写一个程序，找到两个单链表相交的起始节点
+ */
+public class question160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode tmp1 = headA, tmp2 = headB;
+        while (tmp1 != tmp2) {
+            tmp1 = tmp1 == null ? headB : tmp1.next;
+            tmp2 = tmp2 == null ? headA : tmp2.next;
+        }
+        return tmp1;
+    }
+    /**
+     * pA:1->2->3->4->5->6->null->9->5->6->null
+     * pB:9->5->6->null->1->2->3->4->5->6->null
+     * 两个链表分别连起来（当一个走完时，指向另一个的head），如果链表相交则结尾一定有一段相同，同事遍历链表，出现相同节点返回即可
+     */
+}
